923c1728d705195216f3e48a8d4403c47c647794
MarVinCan <marcus.v.e.candido@gmail.com>
2022-03-22 21:21:12 -0300

    Utilizando Interface Comparable, comparando nome

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,31 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import entities.Employee;
+
 public class Program {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Employee> list = new ArrayList<>();
+		String path = "C:\\temp\\in.txt";
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String employeeCSV= br.readLine();
+			while (employeeCSV != null) {
+				String[] filds = employeeCSV.split(","); 
+				list.add(new Employee(filds[0],Double.parseDouble(filds[1])));
+				employeeCSV = br.readLine();
+			}
+			Collections.sort(list);
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", "+ emp.getSalary());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
-
 }
